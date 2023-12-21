224e1a9bff73db80ddb9275ccf22eaba4cbba1f7
henrique1232H <henriqueau111@gmail.com>
2023-12-20 23:41:47 -0300

    ajust bugs

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,12 @@
 let count = 0;
 
 const takeQuerySelector = () => {
-    const p = document.querySelectorAll("p")[1];
     const buttonGame = document.querySelectorAll("button")[1];
-    const title = document.querySelector("div.title");
-    const position = document.querySelector("div.position");
-    const divResult = document.querySelector("div.result");
+    const title = document.querySelector(".title");
+    const position = document.querySelector(".position");
+    const divResult = document.querySelector(".result");
     const buttonBack = document.querySelectorAll("button")[0];
+    const p = title.querySelector(".p");
     
     
     return {
@@ -20,6 +20,7 @@ const takeQuerySelector = () => {
 }
 
 const takeQuery = takeQuerySelector();
+
 
 const randomNumberFunction = () => {
     return Math.round(Math.random( ) * (10 - 0) + 0)
@@ -52,9 +53,9 @@ const buttonBack = () => {
 
 
     takeQuery.buttonBack.addEventListener("click", () => {
-        takeQuery.divResult.style.display = "none";
-        takeQuery.title.style.display = "block";
-        takeQuery.position.style.display = "block";
+        takeQuery.divResult.classList.add("hide");
+        takeQuery.title.classList.remove("hide");
+        takeQuery.position.classList.remove("hide");
         resetCount();
         resetP();
         addKeyEnter();
@@ -63,9 +64,9 @@ const buttonBack = () => {
     
     addEventListener("keypress", (e) => {
         if (e.key === "Enter") {
-            takeQuery.divResult.style.display = "none";
-            takeQuery.title.style.display = "block";
-            takeQuery.position.style.display = "block";
+            takeQuery.divResult.classList.add("hide");
+            takeQuery.title.classList.remove("hide");
+            takeQuery.position.classList.remove("hide")
             resetCount();
             resetP();
             addKeyEnter();
@@ -89,20 +90,22 @@ const match = () => {
     const result = randomNumberFunction();
     const check = checkInput();
     
-    if(check > 10) {
+    if(check > 10 || check < 0) {
+
         takeQuery.p.innerHTML = " *Insira um número entre 1 e 10";
         takeQuery.p.style.color = "red";
         return;
     }
+
     
-    count = count + 1;
+    count++;
     
     
     if(result === check) {
         
-        takeQuery.title.style.display = "none";
-        takeQuery.position.style.display = "none";
-        takeQuery.divResult.style.display = "flex";
+        takeQuery.title.classList.add("hide");
+        takeQuery.position.classList.add("hide");
+        takeQuery.divResult.classList.remove("hide");
         
         buttonBack();
         
@@ -111,11 +114,10 @@ const match = () => {
         
         pAnimation();
         takeQuery.p.innerHTML = `Errou! o número é ${result}`;
-        takeQuery.p.style.color = "#34355B";
-        takeQuery.p.style.display = "flex";
-        takeQuery.p.style.justifyContent = " center";
         
     };
+
+
 }
 
 const matchNumber = () => {
@@ -126,14 +128,5 @@ const matchNumber = () => {
     });
 };
 
-const addKeyEnter = () => {
-
-   addEventListener("keypress", (e) => {
-     if(e.key === "Enter"){
-        match();
-     }
-   })
-}
 
 matchNumber();
-addKeyEnter();
